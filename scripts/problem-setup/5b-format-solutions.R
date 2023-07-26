a29258ae6651efd620b6f140eb5535a76012d1e2
milliechapman <milliechapman@gmail.com>
2023-07-26 14:41:40 +0000

    script to easily visualize a single solution

--- scripts/problem-setup/5b-format-solutions.R
+++ scripts/problem-setup/5b-format-solutions.R
@@ -6,7 +6,7 @@ library(raster)
 library(fasterize)
 #rm(list = ls())
 
-targets <- read_csv("data/formatted-data/targets_split_formatted.csv")
+targets <- read_csv("data/data-formatted/targets_split_formatted.csv")
 pu_in_EU <- read_csv("data/formatted-data/pu_in_EU.csv")
 raster <- raster("data/landcover/10km/Corine_2018_cropland.tif")
 
@@ -39,9 +39,9 @@ for (i in 1:length(filelist_temp)) {
   country_contraint <- filenm[[9]]
   scenario <- filenm[[15]]
   future <- str_sub(filenm[[18]], 1,-5)
-  
+
   solution <- read_csv(paste0("data/solutions/sol/", filelist_temp[[i]]))
-  
+
   solution <- solution |>
     rowid_to_column("pu") |>
     dplyr::select(pu, solution_1_z1:solution_1_z26) |>
@@ -49,14 +49,14 @@ for (i in 1:length(filelist_temp)) {
     mutate(zone = str_sub(name, 13)) |>
     dplyr::select(-name) |>
     mutate(zone = as.numeric(zone))
-  
+
   rij_sol <- rij |>
     left_join(solution, by = c("pu", "zone"))
-  
+
   contribution <- rij_sol |>
     mutate(contribution = amount*value) |>
     group_by(species) |>
-    summarise(rep = sum(contribution))|> 
+    summarise(rep = sum(contribution))|>
     left_join(targets) |>
     mutate(shortfall = rep-target) |>
     mutate(shortfall_perc = shortfall/target) |>
@@ -66,12 +66,12 @@ for (i in 1:length(filelist_temp)) {
            future = future) |>
     mutate(target_met = ifelse(shortfall<0,"no", "yes")) |>
     mutate(spp = ifelse(species != 999999, "biodiv", "carbon"))
-  
+
   shortfalls[[i]] <- contribution
 }
 
 
-shortfalls <- bind_rows(shortfalls, .id = "column_label") 
+shortfalls <- bind_rows(shortfalls, .id = "column_label")
 write_csv(shortfalls, "data/solutions/representation_REF_f455_scenarios_version3.csv")
 
 glimpse(shortfalls)
@@ -83,15 +83,15 @@ zones <- read_csv("data/formatted-data/zone_id.csv") |>
   dplyr::select(-c(A,B,C))
 
 
-LC <- read_csv("data/outputs/2-zones/PU_lc_intensity.csv") |> 
-  dplyr::select(-Status) |>  
+LC <- read_csv("data/outputs/2-zones/PU_lc_intensity.csv") |>
+  dplyr::select(-Status) |>
   pivot_longer(-PUID) |> rename(pu = PUID) |>
   left_join(pu_in_EU) |>
   rename(id = EU_id) |>
   dplyr::select(-c(pu, nuts2id)) |>
   drop_na(id) |> rename(zone = name) |>
   rename(pu = id) |>
-  left_join(zones) |> 
+  left_join(zones) |>
   dplyr::select(-zone) |>
   rename(zone = id)
 
@@ -101,7 +101,7 @@ rij_sol <- rij |>
 contribution <- rij_sol |>
   mutate(contribution = amount*value) |>
   group_by(species) |>
-  summarise(rep = sum(contribution, na.rm = TRUE))|> 
+  summarise(rep = sum(contribution, na.rm = TRUE))|>
   left_join(targets) |>
   mutate(shortfall = rep-target) |>
   mutate(shortfall_perc = shortfall/target) |>
@@ -117,17 +117,17 @@ write_csv(contribution, "data/solutions/representation_IC.csv")
 
 
 ## Save as raster stack
-## 
-## 
-## 
+##
+##
+##
 #### plotting data
 
-zone_id <- read_csv("data/formatted-data/zone_id.csv") 
+zone_id <- read_csv("data/formatted-data/zone_id.csv")
 zone_id$zone
 
 MAES <- readRDS("data/EEA_habitatpreferences.rds") |>
   filter(typeasso %in% c("Preferred", "Suitable")) |>
-  mutate(maes_label = recode(maes_label, 
+  mutate(maes_label = recode(maes_label,
                              `Sparserly vegetated land` = "SparseVeg",
                              `Heathland and shrub` = "HeathlandShrub",
                              `Rivers and lakes` = "RiversLakes",
@@ -142,16 +142,16 @@ PU_template <- raster("data/landcover/10km/Corine_2018_cropland.tif") |>
   st_as_sf() |>
   #st_transform(crs = st_crs(natura)) |>
   dplyr::mutate(id = seq(1:length(geometry))) |>
-  dplyr::select(-Corine_2018_cropland) 
+  dplyr::select(-Corine_2018_cropland)
 raster <- raster("data/landcover/10km/Corine_2018_cropland.tif")
 
 pu_in_EU <- read_csv("data/formatted-data/pu_in_EU.csv")
 
-PU_plot <- pu_in_EU |> rename(id = pu) |> #plot_data |> 
+PU_plot <- pu_in_EU |> rename(id = pu) |> #plot_data |>
   left_join(PU_template) |>
   dplyr::select(EU_id, geometry) |> st_as_sf()
 
-PU_plot <- fasterize(st_as_sf(PU_plot), raster, field = "EU_id") 
+PU_plot <- fasterize(st_as_sf(PU_plot), raster, field = "EU_id")
 
 bioregion <-read_csv("data/formatted-data/pu_in_EU_BR.csv") |>
   dplyr::select(pu, BR_ID) |>
@@ -162,39 +162,39 @@ country <- read_csv("data/formatted-data/linear_constraints/pu_crop_high_budget_
   mutate(country = substr(NUTS_ID, start = 1, stop = 2))|>
   rename(id = pu) |> dplyr::select(-NUTS_ID)
 
-PU_template_EU <- pu_in_EU |> 
-  rename(id = pu) |> #plot_data |> 
-  left_join(PU_template) |> 
+PU_template_EU <- pu_in_EU |>
+  rename(id = pu) |> #plot_data |>
+  left_join(PU_template) |>
   dplyr::select(-id) |>
   rename(id = EU_id)
 
 filelist_temp <- list.files("data/solutions/sol/") #, pattern = c("*_ref.csv|*_f455.csv"))
 solution_scenarios <- list()
 zones <- read_csv("data/formatted-data/zone_id.csv") |>
-  mutate(name = paste0("z", id)) 
+  mutate(name = paste0("z", id))
 #  join targets and spp info
-#  
+#
 for (i in 1:length(filelist_temp)) {
   name <- substr(filelist_temp[[i]],1,nchar(filelist_temp[[i]])-4)
   solution <- read_csv(paste0("data/solutions/sol/", filelist_temp[[i]])) |>
     dplyr::select(id, solution_1_z1:solution_1_z26)
-  
+
   colnames(solution) <- c("id", (zones$zone))
-  
-  solution_table <- pu_in_EU |> rename(id = pu) |> #plot_data |> 
-    left_join(PU_template) |> 
+
+  solution_table <- pu_in_EU |> rename(id = pu) |> #plot_data |>
+    left_join(PU_template) |>
     dplyr::select(-id) |>
     rename(id = EU_id) |>
-    left_join(solution) |> 
-    pivot_longer(-c(nuts2id:geometry)) 
-  
-  solution_table_plot <- solution_table |> 
+    left_join(solution) |>
+    pivot_longer(-c(nuts2id:geometry))
+
+  solution_table_plot <- solution_table |>
     mutate(zone = name) |>
     separate(name, c('maes_label', 'action', 'place'), sep = "_")
-  
+
   solution_raster <- fasterize(st_as_sf(solution_table_plot), raster, field = "value", by = "zone")
   solution_raster <- rast(solution_raster)
-  
+
   writeRaster(solution_raster, paste0("data/solutions/rasters/", name, ".tif"), overwrite = TRUE)
 }
 
@@ -213,7 +213,7 @@ write_csv(bioregion, "data/plotting/bioregion_code.csv")
 # countries
 nuts2_shp <- st_read("data/EU_NUTS2_GLOBIOM/EU_GLOBIOM_NUTS2.shp") |>
   mutate(country = substr(NUTS2, start = 1, stop = 2)) |>
-  dplyr::select(country) 
+  dplyr::select(country)
 
 country_code <- as_tibble(nuts2_shp) |>
   dplyr::select(-geometry) |> unique() |>
@@ -228,7 +228,7 @@ species_id$speciesname <- gsub(" ", "_", species_id$speciesname, fixed=TRUE)
 
 MAES <- readRDS("data/EEA_habitatpreferences.rds") |>
   filter(typeasso %in% c("Preferred", "Suitable")) |>
-  mutate(maes_label = recode(maes_label, 
+  mutate(maes_label = recode(maes_label,
                              `Sparserly vegetated land` = "SparseVeg",
                              `Heathland and shrub` = "HeathlandShrub",
                              `Rivers and lakes` = "RiversLakes",
@@ -256,24 +256,24 @@ for(i in 1:length(files)){
   solution <- read_csv(paste0("data/solutions/sol/", nm)) |>
     dplyr::select(id, solution_1_z1:solution_1_z26)
   colnames(solution) <- c("id", (zone_id$zone))
-  
-  solution_table <- pu_in_EU |> 
-    rename(id = pu) |> #plot_data |> 
-    #left_join(PU_template) |> 
+
+  solution_table <- pu_in_EU |>
+    rename(id = pu) |> #plot_data |>
+    #left_join(PU_template) |>
     #left_join(bioregion) |>
     dplyr::select(-id) |>
     rename(id = EU_id) |>
-    left_join(solution) |> 
-    pivot_longer(-c(nuts2id:id)) 
-  
-  solution_table_plot <- solution_table |> 
+    left_join(solution) |>
+    pivot_longer(-c(nuts2id:id))
+
+  solution_table_plot <- solution_table |>
     mutate(zone = name) |>
     separate(name, c('maes_label', 'intensity', 'action'), sep = "_") |>
     mutate(carbon = carbon,
            country_TF = country_TF,
            scenario = scenario,
            future = future)
-  
+
   plot_table[[i]] <- solution_table_plot
 }
 
